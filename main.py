c42b4b9a20570ca7c8c03de5c630f1a27246efb0
Harvey George Young <harveygyoung@outlook.com>
2021-03-02 01:15:45 +0000

    Fully complete player search.
    player search is entirely done. It displays name, position, program and the price as an int.

--- main.py
+++ main.py
@@ -22,15 +22,43 @@ def playerSearch():
     print("Second name search: (Leave blank if none)")
     userSecondName = input()
 
-    pageSearchSource = requests.get("https://www.muthead.com/21/players/?name__icontains=" + userFirstName + "%20"+ userSecondName + "&page=1").text
-    pageSearchSoup = BeautifulSoup(pageSearchSource, 'lxml')
+    pageSearchSource = "https://www.muthead.com/21/players/?name__icontains=" + userFirstName + "%20"+ userSecondName + "&page=1&summary_price__gte=1"
+    pageSearchRequest = requests.get(pageSearchSource).text
+    pageSearchSoup = BeautifulSoup(pageSearchRequest, 'lxml')
     numberOfPages = pageSearchSoup.find('ul', class_="pagination").text
     numberOfPages = numberOfPages.replace(" ", "")
     numberOfPages = numberOfPages.replace("of", "")
     numberOfPages = numberOfPages[10:]
     numberOfPages = int(numberOfPages)
 
-    print(str(numberOfPages))
+    print(str(numberOfPages) + " number of pages")
+
+    for i in range(numberOfPages + 1):      # Number of pages plus one 
+
+        playerSearchSource = "https://www.muthead.com/21/players/?name__icontains=" + userFirstName + "%20"+ userSecondName + "&page={}&summary_price__gte=1".format(i)
+        playerSearchRequest = requests.get(playerSearchSource).text
+        playerSearchSoup = BeautifulSoup(playerSearchRequest, 'lxml')
+
+        for a in playerSearchSoup.find_all('a', href=True, class_="player-listing__link"):
+
+            playerName = a.find('div', class_="list-info-player__player-name")
+            playerName = playerName.text
+            playerName = playerName.replace("  ", "")
+            playerName = playerName.replace("\n", "")
+
+            playerDetails = a.find('div', class_="list-info-player__player-info")
+            playerDetails = playerDetails.text
+            playerDetails = playerDetails.replace("  ", "")
+            playerDetails = playerDetails.replace("\n", "")
+
+            playerPrice = a.find('div', class_="player-listing__price-value")
+            playerPrice = playerPrice.text
+            playerPrice = playerPrice.replace("K", "")
+            playerPrice = float(playerPrice)
+            playerPrice = playerPrice * 1000
+            playerPrice = int(playerPrice)
+
+            print(playerName + " - " + playerDetails + " - " + str(playerPrice))
 
 def main():
     menu()
